c76c44778fa95b34234415c69d6fa68357a12501
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-05-22 17:31:27 +0200

    :rocket: add framerate limiter

--- src/browser.ts
+++ src/browser.ts
@@ -1,8 +1,14 @@
+import { delay } from "./promise"
+
 export function removeHash() {
-  const [ url ] = window.location.toString().split("#")
+  const [url] = window.location.toString().split("#")
   window.history.replaceState({}, document.title, url)
 }
 
 export async function nextFrame() {
   return new Promise<number>(res => window.requestAnimationFrame(res))
+}
+
+export async function limitFramerate(ms: number) {
+  await Promise.all([nextFrame(), delay(ms)])
 }
