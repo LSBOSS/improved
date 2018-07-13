a1f70951b4051f0be18aa2d8d6cb7ba4fc9ae976
Christoph Reinbothe <c_k_r@gmx.de>
2018-07-13 18:10:26 +0200

    :sparkles: Adding lerp function to math

--- src/math.ts
+++ src/math.ts
@@ -1,3 +1,7 @@
 export function clamp(val: number, min: number, max: number) {
   return Math.min(Math.max(min, val), max)
 }
+
+export function lerp(from: number, to: number, t: number) {
+  return from + clamp(t, 0, 1) * (to - from)
+}
