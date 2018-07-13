cfedb64a496cfdc521a60c151757abc0e63c4be5
Christoph Reinbothe <c_k_r@gmx.de>
2018-07-13 18:10:54 +0200

    :recycle: Adding index.ts for having every function in global name space

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export {}
+export * from "./animation"
+export * from "./fs"
+export * from "./oauth2"
+export * from "./process"
+export * from "./browser"
+export * from "./collections"
+export * from "./json"
+export * from "./math"
+export * from "./promise"
+export * from "./time"
+export * from "./types"
