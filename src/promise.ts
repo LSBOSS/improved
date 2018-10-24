c629b79f7c0c4f16d638a58cee7a58c454823f08
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-10-24 22:06:38 +0200

    :rocket: add series for promise handling

--- src/promise.ts
+++ src/promise.ts
@@ -1,3 +1,17 @@
 export async function delay(ms: number) {
   return new Promise<void>(res => setTimeout(res, ms))
 }
+
+export async function series<T, R>(arr: T[], cb: (item: T, index?: number, arr?: T[]) => Promise<R>): Promise<R[]> {
+  const ret: R[] = []
+  let i = 0
+
+  await arr.reduce(async (a, v) => {
+    await a
+    const result = await cb(v, i, arr)
+    i++
+    ret.push(result)
+  }, Promise.resolve())
+
+  return ret
+}
